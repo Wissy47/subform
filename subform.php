c387db06c3dfa4280b62e7e05ff62747f3d0f4e3
wisdom ighofose <wissy47@gmail.com>
2025-01-29 19:08:24 +0100

    initial commit

---
+++ subform.php
@@ -0,0 +1,31 @@
+<?php 
+
+/*
+Plugin Name: Sub Form
+Description: This is a plugin to display 
+Version: 1.0.0
+Author: Wisdom
+*/
+
+
+// Add Shortcode
+function subform() {
+
+    // Get the current user's display name
+    $user_display_name = wp_get_current_user()->user_firstname;
+
+    // Get the blog name
+    $blog_name = get_bloginfo( 'name' );
+
+    // Build the form output
+    $output = '<p>Hey ' . esc_html($user_display_name) . ', welcome to ' . esc_html($blog_name) . '! You can subscribe to our newsletter here:</p>';
+    $output .= '<form action="/thank-you">';
+    $output .= '<label for="email">Enter your email:</label>';
+    $output .= '<input type="email" id="email" name="email">';
+    $output .= '<input type="submit" value="Submit">';
+    $output .= '</form>';
+
+    // Return the output instead of echo
+    return $output;
+}
+add_shortcode( 'subscriptionform', 'subform' );
